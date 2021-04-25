3737cb07735ed1fb4bab7b2559127951cdc0d6ae
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-25 16:07:02 +0300

    Save instance state in bundle

--- app/src/main/java/io/github/kirillmokretsov/draganddraw/BoxDrawingView.kt
+++ app/src/main/java/io/github/kirillmokretsov/draganddraw/BoxDrawingView.kt
@@ -4,14 +4,20 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.PointF
+import android.os.Bundle
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import com.google.gson.Gson
 
 private const val TAG = "BoxDrawingView"
+private const val BUNDLE_PARENT = "bundleOfParent"
+private const val BUNDLE_BOXEN = "bundleOfBoxen"
 
-class BoxDrawingView(context: Context, attributeSet: AttributeSet? = null) : View(context, attributeSet) {
+class BoxDrawingView(context: Context, attributeSet: AttributeSet? = null) :
+    View(context, attributeSet) {
 
     private var currentBox: Box? = null
     private val boxen = mutableListOf<Box>()
@@ -52,6 +58,15 @@ class BoxDrawingView(context: Context, attributeSet: AttributeSet? = null) : Vie
 
         return true
     }
+
+    override fun onSaveInstanceState(): Bundle =
+        Bundle().apply {
+            putParcelable(BUNDLE_PARENT, super.onSaveInstanceState())
+            for (i in 0 until boxen.size) {
+                putSerializable(BUNDLE_BOXEN + i, Gson().toJson(boxen[i]))
+            }
+        }
+
 
     override fun onDraw(canvas: Canvas) {
         // Fill the background
