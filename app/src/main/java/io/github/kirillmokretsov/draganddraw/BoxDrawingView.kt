b54039772a3713eaecd4fc24803365b2f9d4b07b
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-04-25 09:50:25 +0300

    Create BoxDrawingView.kt

---
+++ app/src/main/java/io/github/kirillmokretsov/draganddraw/BoxDrawingView.kt
@@ -0,0 +1,7 @@
+package io.github.kirillmokretsov.draganddraw
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class BoxDrawingView(context: Context, attributeSet: AttributeSet? = null) : View(context, attributeSet)
